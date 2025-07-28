e204d2689cb2e6a1103d06d3fb8aa372e30ea377
Odaliz Aracely <balsecaodaliz@gmail.com>
2025-07-28 10:33:42 -0500

    Pagina Principal Creacion de login

---
+++ src/vista/Home.java
@@ -0,0 +1,24 @@
+package vista;
+
+import javax.swing.*;
+
+public class Home {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JPanel panelAgendar;
+    private JLabel iconoAgendarCita;
+    private JTextField textPropietario;
+    private JTextField textMascota;
+    private JComboBox comboBoxHora;
+    private JTextField textMotivo;
+    private JButton agendarCitaButton;
+    private JPanel panelLogin;
+    private JButton iniciarSesionButton;
+    private JComboBox comboBoxUsuario;
+    private JPasswordField contraseña;
+    private JButton limpiarButton;
+
+    private void createUIComponents() {
+
+    }
+}
